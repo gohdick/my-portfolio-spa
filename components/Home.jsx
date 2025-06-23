46135b72215ab46cdad40c07954e0b58c36a2ac7
gohdick <thirasaksimtong@gmail.com>
2025-06-24 00:17:47 +0700

    ✨ Update project all file

--- components/Home.jsx
+++ components/Home.jsx
@@ -189,7 +189,7 @@ export default function Home() {
                 className="w-full h-full relative"
               >
                 <motion.img
-                  src="/Cloudflare-Logo.wine.png"
+                  src="/my_picture_v2.png"
                   alt="Goh Dik"
                   className="w-full h-full object-cover"
                   whileHover={{ scale: 1.05 }}
